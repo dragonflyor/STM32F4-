c77614706cbc60aa03bc821a36c8a6b7491845f2
dragonflor <1137176847@qq.com>
2016-01-15 20:13:19 +0800

    添加工程测试串口任务

---
+++ 例1_UCOSIII任务_串口任务/USER/main.c
@@ -0,0 +1,51 @@
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "led.h"
+#include "lcd.h"
+#include "includes.h"
+
+#include "APPS.h"
+
+
+//主函数
+int main(void)
+{
+	OS_ERR err;
+	CPU_SR_ALLOC();
+	
+	delay_init(168);  //时钟初始化
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//中断分组配置
+	uart_init(115200);   //串口初始化
+	
+	INTX_DISABLE();		//关中断,防止滴答定时器对外设初始化的打扰
+	LED_Init();         //LED初始化	
+	LCD_Init();			//LCD初始化	
+	
+	POINT_COLOR = RED;
+	LCD_ShowString(30,10,200,16,16,"Explorer STM32F4");	
+	LCD_ShowString(30,30,200,16,16,"UCOSIII Examp 6-1");
+	LCD_ShowString(30,50,200,16,16,"Task Creat and Del");
+	LCD_ShowString(30,70,200,16,16,"ATOM@ALIENTEK");
+	LCD_ShowString(30,90,200,16,16,"2014/11/25");
+	INTX_ENABLE();		//开中断
+	
+	OSInit(&err);		    //初始化UCOSIII
+	OS_CRITICAL_ENTER();	//进入临界区			 
+	//创建开始任务
+	OSTaskCreate((OS_TCB 	* )&StartTaskTCB,		//任务控制块
+				 (CPU_CHAR	* )"start task", 		//任务名字
+                 (OS_TASK_PTR )start_task, 			//任务函数
+                 (void		* )0,					//传递给任务函数的参数
+                 (OS_PRIO	  )START_TASK_PRIO,     //任务优先级
+                 (CPU_STK   * )&START_TASK_STK[0],	//任务堆栈基地址
+                 (CPU_STK_SIZE)START_STK_SIZE/10,	//任务堆栈深度限位
+                 (CPU_STK_SIZE)START_STK_SIZE,		//任务堆栈大小
+                 (OS_MSG_QTY  )0,					//任务内部消息队列能够接收的最大消息数目,为0时禁止接收消息
+                 (OS_TICK	  )0,					//当使能时间片轮转时的时间片长度，为0时为默认长度，
+                 (void   	* )0,					//用户补充的存储区
+                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR, //任务选项
+                 (OS_ERR 	* )&err);				//存放该函数错误时的返回值
+	OS_CRITICAL_EXIT();	//退出临界区	 
+	OSStart(&err);      //开启UCOSIII
+}
